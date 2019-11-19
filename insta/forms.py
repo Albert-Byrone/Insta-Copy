bb52204bfac8831ee52703425c8f29d470904083
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-19 06:52:29 +0300

    create a user profiloe mode form

--- insta/forms.py
+++ insta/forms.py
@@ -12,3 +12,11 @@ class SignUpForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
 
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
