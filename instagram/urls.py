e6cde4d86ed887cab710d7cd13846d14ac5654ab
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-19 14:05:18 +0300

    Make update

--- instagram/urls.py
+++ instagram/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     url(r'^accounts/',include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
