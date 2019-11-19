99ce620ee32868fe75494aa9d7656aa46a17976b
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-19 06:50:56 +0300

    register the modules to the admin section

---
+++ insta/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Profile, Post, Comment, Follow
+
+admin.site.site_header = 'Admin Dashboard'
+admin.site.site_title = 'InstaCopy'
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Follow)
+
