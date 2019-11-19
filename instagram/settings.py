1e231e784797d939b558a944e859440d391dc5b6
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-19 16:32:58 +0300

    deploy app

--- instagram/settings.py
+++ instagram/settings.py
@@ -91,7 +91,7 @@ ROOT_URLCONF = 'instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
